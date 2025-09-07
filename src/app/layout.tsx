a595e6cb702aa4ccdcdac965bd460f5b3427a897
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-09-07 19:01:27 +0900

    feature:button#7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Button from "@/component/Button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Button children="home" />
       </body>
     </html>
   );
--- src/component/Button/index.tsx
+++ src/component/Button/index.tsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { isContext } from "vm";
+import styles from "./index.module.css";
+import { ChevronRight } from 'lucide-react';
+
 interface ButtonProps {
     children: React.ReactNode;
+    color?: string;
+    icon?: React.ReactNode;
+    
+    
 }
 
-const Button = ({children}: ButtonProps) => {
-    return <button>{children}</button>;
+const Button = ({children, color, icon, arrow}: ButtonProps) => {
+    return <button className={styles.button} style={{backgroundColor: color}}>
+    <div className={styles.icon}>{icon}</div>
+    <div className={styles.moji}>{children}</div>
+    <div className={styles.arrow}><ChevronRight size={30}></ChevronRight></div>
+    </button>
 };
 
 export default Button;
