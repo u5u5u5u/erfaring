4836664d64fdd045e4d62975dab8063280c69b1b
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-09-07 15:38:54 +0900

    edit:aiributton

--- src/app/airi2/page.tsx
+++ src/app/airi2/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/component/Button";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Button children={"airi"} />
         <Image
           className={styles.logo}
           src="/next.svg"
