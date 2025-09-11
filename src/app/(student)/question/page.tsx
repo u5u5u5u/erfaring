79ae8ec1997131b54cdd9b0a036a3e0124212ca5
U5 <kyutech.joko@gmail.com>
2025-09-11 21:00:35 +0900

    add:問い追加ボタン追加#18

--- src/app/(student)/question/page.tsx
+++ src/app/(student)/question/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import AddQuestionButton from "@/component/AddQuestionButton";
+import styles from "./page.module.css";
+
 export default function QuestionPage() {
-  return <div>探究ノート</div>;
+  return (
+    <div className={styles.container}>
+      <h1>探究ノート</h1>
+      <div className={styles.buttonContainer}>
+        <AddQuestionButton onClick={() => console.log("新しい問いを追加")} />
+      </div>
+    </div>
+  );
 }
