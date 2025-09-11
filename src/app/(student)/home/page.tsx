9f1f9488376d855d14d46a3bbf2f97eca5a6ddb2
U5 <kyutech.joko@gmail.com>
2025-09-11 20:21:18 +0900

    fix:見た目少し修正#17

--- src/app/(student)/home/page.tsx
+++ src/app/(student)/home/page.tsx
@@ -4,6 +4,7 @@ import { BookOpen, Swords } from "lucide-react";
 import Quest from "@/component/Quest";
 import type { quest } from "@/types/quest";
 import type { Question } from "@/types/question";
+import styles from "./page.module.css";
 
 export default function HomePage() {
   const dummyUser: User = {
@@ -28,11 +29,6 @@ export default function HomePage() {
       name: "図書館の人",
       title: "未来の図書館を考えよう",
     },
-    {
-      id: "3",
-      name: "消防署の人",
-      title: "防災マップを作ろう",
-    },
   ];
 
   const dummyQuestions: Question[] = [
@@ -44,50 +40,46 @@ export default function HomePage() {
       id: "2",
       title: "なぜ海は塩辛いの？",
     },
-    {
-      id: "3",
-      title: "なぜ葉っぱは緑色なの？",
-    },
   ];
 
   return (
-    <div>
-      <div>
-        <h1>{dummyUser.name}の学びのあしあと</h1>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>
+          {dummyUser.name} の学びのあしあと
+        </h1>
         <p>
           {dummyUser.schoolId}
           {dummyUser.grade}年生
         </p>
       </div>
-      <div>
-        <div>
-          <Button color="#2463EB" icon={<Swords />} link="/quest">
-            クエストに挑戦
-          </Button>
-          <Button color="#21C55D" icon={<BookOpen />} link="/question">
-            問いを探究
-          </Button>
-        </div>
-        <div>
-          <h2>参加中のクエスト</h2>
-          <ul>
-            {dummyQuests.map((quest) => (
-              <li key={quest.id}>
-                <Quest theme={quest.title} people={quest.name} />
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h2>最近の問い</h2>
-          <ul>
-            {dummyQuestions.map((question) => (
-              <li key={question.id}>
-                <Quest theme={question.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className={styles.buttons}>
+        <Button color="#2463EB" icon={<Swords />} link="/quest">
+          クエストに挑戦
+        </Button>
+        <Button color="#21C55D" icon={<BookOpen />} link="/question">
+          問いを探究
+        </Button>
+      </div>
+      <div className={`${styles.section} ${styles.questSection}`}>
+        <h2>参加中のクエスト</h2>
+        <ul>
+          {dummyQuests.map((quest) => (
+            <li key={quest.id}>
+              <Quest theme={quest.title} people={quest.name} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={`${styles.section} ${styles.questionSection}`}>
+        <h2>最近の問い</h2>
+        <ul>
+          {dummyQuestions.map((question) => (
+            <li key={question.id}>
+              <Quest theme={question.title} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
