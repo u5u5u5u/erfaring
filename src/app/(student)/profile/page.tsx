1c0f69bc08b8a2ae737b5c4f6f5952a10ce3e161
U5 <kyutech.joko@gmail.com>
2025-09-12 17:33:48 +0900

    add:ProfileIconコンポーネント作成#20

--- src/app/(student)/profile/page.tsx
+++ src/app/(student)/profile/page.tsx
@@ -1,3 +1,21 @@
+import ProfileIcon from "@/components/ProfileIcon";
+import type { User } from "@/types/user";
+
 export default function ProfilePage() {
-  return <div>プロフィール</div>;
+  const dummyUser: User = {
+    id: "1",
+    name: "ゆうご",
+    email: "yugo@example.com",
+    password: "password123",
+    imageUrl: "/vercel.svg",
+    role: "student",
+    schoolId: "〇〇市立〇〇小学校",
+    grade: 5,
+  };
+
+  return (
+    <div>
+      <ProfileIcon user={dummyUser} />
+    </div>
+  );
 }
