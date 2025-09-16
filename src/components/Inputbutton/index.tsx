b8cfeaf6e2d45a376211802e0e5da03e155cc422
U5 <kyutech.joko@gmail.com>
2025-09-16 22:12:42 +0900

    fix:チャットのデザインの修正#52

--- src/components/Inputbutton/index.tsx
+++ src/components/Inputbutton/index.tsx
@@ -1,17 +1,25 @@
-import styles from "./index.module.css"
-import { Send } from 'lucide-react';
+import styles from "./index.module.css";
+import { Send } from "lucide-react";
 
-interface InputbuttonProps{
-    text?: string;
-};
-
-const Inputbutton = ({text}:InputbuttonProps) =>{
-    return(
-        <div className={styles.Inputbutton}>
-            <input className={styles.form} type="text" placeholder="気づいたこと調べたことを記録しよう"　size={40}>{text}</input>
-            <button className={styles.button}><Send className={styles.Send}></Send></button>
-
-        </div>
-    );
+interface InputbuttonProps {
+  text?: string;
 }
+
+const Inputbutton = ({ text }: InputbuttonProps) => {
+  return (
+    <div className={styles.inputButton}>
+      <input
+        className={styles.form}
+        type="text"
+        placeholder="気づいたこと調べたことを記録しよう"
+        size={40}
+      >
+        {text}
+      </input>
+      <button className={styles.button}>
+        <Send className={styles.send} size={35}></Send>
+      </button>
+    </div>
+  );
+};
 export default Inputbutton;
