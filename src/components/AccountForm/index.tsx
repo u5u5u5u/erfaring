3544dc0abeedbea5995dffb1ca40665885ad5036
U5 <kyutech.joko@gmail.com>
2025-09-20 01:25:20 +0900

    add:ユーザーアイコン保存#50

--- src/components/AccountForm/index.tsx
+++ src/components/AccountForm/index.tsx
@@ -2,8 +2,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { type User } from "@supabase/supabase-js";
-
-// ...
+import Avatar from "@/components/Avatar";
 
 export default function AccountForm({ user }: { user: User | null }) {
   const supabase = createClient();
@@ -77,7 +76,15 @@ export default function AccountForm({ user }: { user: User | null }) {
 
   return (
     <div className="form-widget">
-      {/* ... */}
+      <Avatar
+        uid={user?.id ?? null}
+        url={avatar_url}
+        size={150}
+        onUpload={(url) => {
+          setAvatarUrl(url);
+          updateProfile({ fullname, username, website, avatar_url: url });
+        }}
+      />
 
       <div>
         <label htmlFor="email">Email</label>
